fe6e9fe3a2fc07fdfe9cb0d1b9b10324ec5e0f56
Emir Pasic <emirpasic83@gmail.com>
2017-03-08 08:56:29 +0100

    - Override the default console logging

--- util/logger.js
+++ util/logger.js
@@ -1,5 +1,6 @@
 const config = rr('config/config');
 const winston = require('winston');
+const util = require('util');
 
 const loggers = [];
 
@@ -25,6 +26,26 @@ if (config.logger.file) {
 const logger = new (winston.Logger)({
     transports: loggers
 });
+
+// Override the default console logging
+const formatArgs = (args) => {
+    return [util.format.apply(util.format, Array.prototype.slice.call(args))];
+};
+console.log = function () {
+    logger.info.apply(logger, formatArgs(arguments));
+};
+console.info = function () {
+    logger.info.apply(logger, formatArgs(arguments));
+};
+console.warn = function () {
+    logger.warn.apply(logger, formatArgs(arguments));
+};
+console.error = function () {
+    logger.error.apply(logger, formatArgs(arguments));
+};
+console.debug = function () {
+    logger.debug.apply(logger, formatArgs(arguments));
+};
 
 module.exports = logger;
 module.exports.loggers = loggers;
